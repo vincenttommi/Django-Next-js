830766c5cf4130754952c1a16fb4a75fea9b4123
vincenttommi <vincenttommi@gmail.com>
2025-02-17 13:41:31 +0300

    navbar completed

--- frontend/src/app/components/common/Header.js
+++ frontend/src/app/components/common/Header.js
@@ -3,31 +3,31 @@ const  pacifico = Pacifico({ subsets:['latin'], weight:'400'})
 
 export default function Header(){
     return(
-    <nav className="navbar navbar-expand-lg " style={{'backgroundColor':'red'}}>
+    <nav className="navbar navbar-expand-lg " data-bs-theme="dark" style={{'backgroundColor':'#7f005d'}}>
         <div className="container">
-            <a className={`navbar-brand  ${pacifico.className} fs-4`} href="#">HMS</a>
+            <a className={`navbar-brand  ${pacifico.className} fs-4`}  style={{'color':'#7ff66d6'}}href="#">HMS</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span> 
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item">
-                    <a className="nav-link " aria-current="page" href="#">Home</a>
+                    <li className="nav-item  me-3">
+                    <a className="nav-link   text-white fw-bold" aria-current="page" href="#">Home</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Gallery</a>
+                    <li className="nav-item me-3">
+                    <a className="nav-link text-white fw-bold" href="#">Gallery</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Services</a>
+                    <li className="nav-item me-3">
+                    <a className="nav-link  text-white  fw-bold" href="#">Services</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">About Us</a>
+                    <li className="nav-item me-3">
+                    <a className="nav-link   text-white fw-bold" href="#">About Us</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Contact</a>
+                    <li className="nav-item me-3">
+                    <a className="nav-link   text-white  fw-bold"  href="#">Contact</a>
                     </li>
-                    <li className="nav-item">
-                    <a className="btn btn-danger btn-sm mt-1" href="#">Book Event</a>
+                    <li className="nav-item me-3">
+                    <a className="btn mt-1  text-white fw-bold"   style={{'backgroundColor':'#68004c'}} href="#">Book Event</a>
                     </li>
                 </ul>
             </div>
