2b15c9d31a328e09b1466c70dbbdfb8d7cda7f0a
vincenttommi <vincenttommi@gmail.com>
2025-02-20 22:55:58 +0300

    correcting room types

--- frontend/src/app/components/HomeServices.js
+++ frontend/src/app/components/HomeServices.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function HomeServices() {
     return (
-        <section className="container my-5" >
-            <h3 className="my-5 text-center " style={{color:'#68004c'}}  >Services</h3>
+        <section className="container " >
+            <div className="row">
             <div className="row text-center">  
                 <div className="col-4">
                     <div className="card">
                         <Link href='/room-types'>
-                        <img src="1.jpg" className="card-img-top" alt="Room Types" />
+                        <img src="room1.jpg" className="card-img-top" alt="Room Types" />
                         </Link>
                         <div className="card-body hms-bg-normal  text-white">
                             <h5 className="card-title" ><Link href='/room-types' className="text-white">Room Types</Link></h5>
@@ -19,7 +19,7 @@ export default function HomeServices() {
                 <div className="col-4">
                     <div className="card">
                         <Link href='/room-types'>
-                        <img src="1.jpg" className="card-img-top" alt="Room Types" />
+                        <img src="room1.jpg" className="card-img-top" alt="Room Types" />
                         </Link>
                         <div className="card-body hms-bg-normal text-white">
                             <h5 className="card-title">Room Types</h5>
@@ -29,13 +29,15 @@ export default function HomeServices() {
                 <div className="col-4">
                     <div className="card">
                         <Link href='/room-types'>
-                        <img src="1.jpg" className="card-img-top" alt="Room Types" />
+                        <img src="room1.jpg" className="card-img-top" alt="Room Types" />
                         </Link>
                         <div className="card-body hms-bg-normal text-white ">
                             <h5 className="card-title">Room Types</h5>
                         </div>
                     </div>    
                 </div>
+            </div>
+
             </div>
         </section>
     );
--- frontend/src/app/room-types/page.js
+++ frontend/src/app/room-types/page.js
@@ -1,9 +1,47 @@
-export default function  Page(){
+import Link from "next/link";
 
-    return(
-
-    <section>
-        <h3>All Room Types</h3>
-    </section>
-    ) 
+export default function Page() {
+    return (
+        <section className="container my-5">
+            <h3 className="my-4">Room Types</h3>
+            <div className="row">
+                <div className="col-4">
+                    <div className="card">
+                        <Link href="/room-types">
+                            <img src="room1.jpg" className="card-img-top" alt="Room Types" />
+                        </Link>
+                        <div className="card-body hms-bg-dark text-white">
+                            <h5 className="card-title">
+                                <Link href="/room-types" className="text-white">Room Type 1</Link>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card">
+                        <Link href="/room-types">
+                            <img src="room1.jpg" className="card-img-top" alt="Room Types" />
+                        </Link>
+                        <div className="card-body hms-bg-dark text-white">
+                            <h5 className="card-title">
+                                <Link href="/room-types" className="text-white">Room Type 1</Link>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="card">
+                        <Link href="/room-types">
+                            <img src="room1.jpg" className="card-img-top" alt="Room Types" />
+                        </Link>
+                        <div className="card-body hms-bg-dark text-white">
+                            <h5 className="card-title">
+                                <Link href="/room-types" className="text-white">Room Type 1</Link>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
