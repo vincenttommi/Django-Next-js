c961eddd8d9daec76b134270750f1e93331b5953
vincenttommi <vincenttommi@gmail.com>
2025-04-07 23:41:53 +0300

    correcting code

--- frontend/src/app/components/RoomType.js
+++ frontend/src/app/components/RoomType.js
@@ -1,32 +1,33 @@
 import Link from "next/link";
 
-export default function RoomType({ item }){
+export default function RoomType({ item }) {
+  const images = item.room_type_images || [];
 
+  let first_image = '/banner1.jpg';
 
-    let images = item.room_type_images;
-    let  first_image = '/banner1.jpg';
+  if (images.length > 0 && images[0].image) {
+    first_image = images[0].image;
+  }
 
-
-    if(images.length > 0 && images[0].image){
-        first_image = images[0].image;  //override if an image exists
-    }
-
-
- 
-    return(
-        <div className="col-4 mb-4">
-            <div className="card">
-                <Link href={`/room-types/${item.title}`}>
-                <img src={first_image}  className="card-img-top" alt="Room Types" style={{ height:"200px",objectFit:"cover"}} />
-                </Link>
-                <div className="card-body hms-color-dark text-white">
-                    <h5 className="card-title">
-
-                        <Link  href={`/room-types/${item.title}`}  className="text-white">{item.title}</Link>
-
-                    </h5>
-                </div>
-            </div>
+  return (
+    <div className="col-4 mb-4">
+      <div className="card">
+        <Link href={`/room-types/${encodeURIComponent(item.title)}`}> {/* Use title as slug */}
+          <img
+            src={first_image}
+            className="card-img-top"
+            alt="Room Types"
+            style={{ height: "200px", objectFit: "cover" }}
+          />
+        </Link>
+        <div className="card-body hms-color-dark text-white">
+          <h5 className="card-title">
+            <Link href={`/room-types/${encodeURIComponent(item.title)}`} className="text-white">
+              {item.title}
+            </Link>
+          </h5>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
