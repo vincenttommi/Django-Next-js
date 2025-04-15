604f9b52a12e6ed83803448a2103ad51cdf8d461
vincenttommi <vincenttommi@gmail.com>
2025-04-15 13:26:57 +0300

    correcting roomtype code

--- frontend/src/app/room-types/[slug]/page.js
+++ frontend/src/app/room-types/[slug]/page.js
@@ -1,49 +1,49 @@
 import RoomTypeImages from "@/app/components/RoomTypeImages";
 
-
-
-async function getData(room_type_id) {
- 
-  const  res = await fetch('http://localhost:8000/rooms/room-types/'+room_type_id, {cache:'no-store'});
+async function getData(room_type_id, page = 1) {
+  const url = `http://localhost:8000/rooms/room-types/${room_type_id}${
+    page ? `?page=${page}` : ""
+  }`;
+  const res = await fetch(url, { cache: "no-store" });
   if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
   const data = await res.json();
   console.log("API Response:", data);
   return data;
 }
 
+export default async function Page({ params, searchParams }) {
+  const room_type_id = params.slug; // Get room_type_id from URL (e.g., /room-types/1)
 
-export default async function Page(){
+  // Await searchParams to resolve dynamic query parameters
+  const resolvedSearchParams = await searchParams;
+  const page = resolvedSearchParams?.page || 1; // Get page, default to 1
 
+  const roomDetail = await getData(room_type_id, page); // Fetch data
 
-
-  const  room_type_id = 1
-  const page  =  searchParams.page;
-  const  roomDetail = await getData(page);
-
-
-
-  return(
-    <section  className="container my-5">
+  return (
+    <section className="container my-5">
       <h3>{roomDetail.title}</h3>
       <div className="row">
         <div className="col-7">
-          <RoomTypeImages/>
-          <h4 className="my-5">Amentities</h4>
+          <RoomTypeImages images={roomDetail.images || []} />
+          <h4 className="my-5">Amenities</h4>
           <div className="row">
             <div className="col-3">
-              <div className="col-3">
-
-                <p>Wifi:<i className="fa fa-check-circle text-success"></i></p>
-                <p>Swimming Pool:<i className="fa fa-check text-success"></i></p>
-                <p>Camera:<i className="fa fa-times-circle text-danger"></i></p>
-
-              </div>
+              <p>
+                Wifi: <i className="fa fa-check-circle text-success"></i>
+              </p>
+              <p>
+                Swimming Pool: <i className="fa fa-check text-success"></i>
+              </p>
+              <p>
+                Camera: <i className="fa fa-times-circle text-danger"></i>
+              </p>
             </div>
-            <h4 className="my-5">Location</h4>
-            <p>Google Map</p>
           </div>
+          <h4 className="my-5">Location</h4>
+          <p>Google Map</p>
         </div>
       </div>
     </section>
-  )
+  );
 }
