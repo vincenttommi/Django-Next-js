b9e7d453f1faa7a3e46008978bd621df644ddfd4
vincenttommi <vincenttommi@gmail.com>
2025-05-16 17:02:26 +0300

    correcting code

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('logout/', views.LogoutUserView.as_view(),  name='logout'),
     path('password-reset-request/', views.PasswordResetRequestView.as_view(), name='PasswordResetRequestView'),
     # path('verify-user-email/', views.VerifyuserEmail.as_view(), name='VerifyuserEmail'),
-    path('api/password-reset-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
-    path('api/set-new-password/', views.SetNewPassword.as_view(), name='set-new-password'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('set-new-password/', views.SetNewPassword.as_view(), name='set-new-password'),
     path('otp-validate/', views.OTPValidationView.as_view(), name='otp-validate')
     
     
